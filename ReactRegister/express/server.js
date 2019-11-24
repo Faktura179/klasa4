1b3212c48a8d7afa261e7461625ac9dcb33c6c28
Faktura179 <rfatula12@gmail.com>
2019-11-24 13:06:59 +0100

    Server in Registerapp

--- ReactRegister/express/server.js
+++ ReactRegister/express/server.js
@@ -22,13 +22,13 @@ app.post("/", function (req, res) {
     else{
         res.send(false)
     }
-    console.log(users)
+    //console.log(users)
     
 })
 app.post("/login",function(req,res){
     var add = true
     for(var i in users){
-        console.log(users[i],req.body,JSON.stringify( users[i] ) == JSON.stringify( req.body ))
+        //console.log(users[i],req.body,JSON.stringify( users[i] ) == JSON.stringify( req.body ))
         if(JSON.stringify(users[i])==JSON.stringify(req.body)){
             res.send(true)
             add=false
@@ -38,7 +38,22 @@ app.post("/login",function(req,res){
         res.send(false)
     }
 })
-
+app.post("/getusers",function(req,res){
+    res.send(users)
+})
+app.post("/delete",function(req,res){
+    var flag = true
+    for(var i in users){
+        if(JSON.stringify(users[i])==JSON.stringify(req.body)){
+            users.splice(i,1)
+            flag=false
+            res.send(true)
+        }
+    }
+    console.log(users)
+    if(flag)
+        res.send(false)
+})  
 app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
